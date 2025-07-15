51c1df01424b3498c7224be9d43aece7998967d0
BartRoosenCampus <bart.roosen@vdabcampus.be>
2025-07-15 14:16:53 +0200

    20250715: not so first commit

---
+++ js/PageHandler.js
@@ -0,0 +1,65 @@
+"use strict";
+
+export class PageHandler {
+    elements;
+
+    constructor(elements) {
+        this.elements = elements;
+        this.drawKeyPad();
+    }
+
+    drawKeyPad() {
+        const keys = [9, 8, 7, 6, 5, 4, 3, 2, 1, 'D', 0, 'S'];
+
+        keys.forEach((key) => {
+            const div = this.make("div");
+            const button = this.make("button");
+
+
+            button.classList.add("key");
+            button.dataset.value = key;
+            button.innerHTML = key.toString();
+
+            switch (key) {
+                case "D":
+                    button.classList.add("functionKey");
+                    button.addEventListener("click", () => {
+                        this.elements.inputThrow.value = "";
+                        this.elements.error.innerHTML = "";
+                    });
+                    break;
+                case "S":
+                    button.classList.add("functionKey");
+                    break;
+                default:
+                    button.addEventListener("click", () => {
+                        const bogy = [163, 166, 169, 172, 173, 175, 176, 178, 179];
+                        let number = this.elements.inputThrow.value;
+
+                        if (typeof number === 'undefined') {
+                            number = 0;
+                        }
+
+                        this.elements.inputThrow.value = `${number}${key}`;
+                        number = parseInt(this.elements.inputThrow.value);
+
+                        if (number > 180 || bogy.includes(number)) {
+                            this.elements.error.innerHTML = `Nummer ${number} kan niet gegooid worden`;
+                            this.elements.inputThrow.value = "";
+                        } else {
+                            this.elements.error.innerHTML = "";
+                        }
+                    });
+                    break;
+            }
+
+
+            div.appendChild(button);
+            this.elements.keyPad.appendChild(div);
+        })
+    }
+
+    make(tag) {
+        return document.createElement(tag);
+    }
+}
--- js/main.js
+++ js/main.js
@@ -2,69 +2,17 @@
 
 import {Training} from "./Training.js";
 import {LocalStorage} from "./LocalStorage.js";
+import {PageHandler} from "./PageHandler.js";
 
 const elements = {
-    trainingNotStartedArea: document.getElementById('trainingNotStartedArea'),
-    trainingStartedArea: document.getElementById('trainingStartedArea'),
-    btnMis: document.getElementById('btnMis'),
-    btnSingle: document.getElementById('btnSingle'),
-    btnDouble: document.getElementById('btnDouble'),
-    btnTriple: document.getElementById('btnTriple'),
-    aim: document.getElementById('aim'),
-    buttonsArea: document.getElementById('buttonsArea'),
-    numbersToSelectArea: document.getElementById('numbersToSelectArea'),
-    btnBack: document.getElementById('btnBack'),
-}
+    keyPad: byId("keyPad"),
+    inputThrow: byId("inputThrow"),
+    error: byId("error"),
+};
 
-const training = new Training(elements);
-const localStorage = new LocalStorage();
+const pageHandler = new PageHandler(elements);
 
-for (let i = 20; i >= 0; i--) {
-    const div = document.createElement('div');
-    const button = document.createElement('button');
 
-    button.innerText = (0 === i) ? "R" : i.toString();
-    button.addEventListener('click', () => {
-        training.setNumber((0 === i) ? Math.floor(Math.random() * 20) + 1 : i);
-        training.start();
-        toggleAreas();
-        localStorage.save('training', training);
-    });
-    div.appendChild(button);
-    elements.numbersToSelectArea.appendChild(div);
-}
-
-elements.btnMis.addEventListener('click', () => {
-    training.addMis();
-    localStorage.save('training', training);
-});
-
-elements.btnSingle.addEventListener('click', () => {
-    training.addSingle();
-    localStorage.save('training', training);
-});
-
-elements.btnDouble.addEventListener('click', () => {
-    training.addDouble();
-    localStorage.save('training', training);
-});
-
-elements.btnTriple.addEventListener('click', () => {
-    training.addTriple();
-    localStorage.save('training', training);
-});
-
-elements.btnBack.addEventListener('click', () => {
-    training.stepBack();
-    localStorage.save('training', training);
-});
-
-function toggleAreas() {
-    if (elements.trainingStartedArea.style.display === 'none') {
-        elements.trainingStartedArea.style.display = '';
-        elements.trainingNotStartedArea.style.display = 'none';
-    } else {
-        elements.trainingStartedArea.style.display = 'none';
-        elements.trainingNotStartedArea.style.display = '';
-    }
+function byId(id) {
+    return document.getElementById(id);
 }
